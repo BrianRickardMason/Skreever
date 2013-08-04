f1a9d7b6487a293dbc9ca2a91facd3357ab34472
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2013-08-04 16:00:06 +0200

    Processor class added.

---
+++ Processor.py
@@ -0,0 +1,21 @@
+import time
+import threading
+
+class Processor(threading.Thread):
+    """Processor class.
+    
+    Attributes:
+        mName: Human readable name.
+    
+    """
+    
+    def __init__(self, aName):
+        self.mName = aName
+        
+        threading.Thread.__init__(self, aName)
+    
+    def run(self):
+        while True:
+            time.sleep(1)
+            print("Processor: " + self.mName)
+            
